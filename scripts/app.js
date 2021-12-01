bb66f2667bb5c12eaf2494ffd5a18a4e461e9ff1
Francisco Oliveira <chicinho.oliveira@gmail.com>
2021-12-01 12:45:04 +0000

    Add operate function

--- scripts/app.js
+++ scripts/app.js
@@ -13,52 +13,65 @@
 ///////////////
 
 
-const add = function() {
-	values = [...arguments];
+function operate(operator, values) {
+    let answer = 0;
 
+    switch(operator) {
+        case "÷":
+            answer = divide(values);
+            break;
+        case "*":
+            answer = multiply(values);
+            break;
+        case "+":
+            answer = add(values);
+            break;
+        case "-":
+            answer = subtract(values);
+            break;
+        case "^":
+            answer = power(values);
+            break;
+        case "!":
+            answer = factorial(values);
+    }  
+    console.log(answer);
+}
+
+const add = function(values) {
   let added = values[0] + values[1];
   return added;
 };
 
-const subtract = function() {
-	values = [...arguments];
-
+const subtract = function(values) {
   let subtracted = values[0] - values [1];
   return subtracted;
 };
 
-const multiply = function() {
-	values = [...arguments];
-
+const multiply = function(values) {
   let multiplied = values[0];
   for (let i=1; i<values.length; i++){
     multiplied *= values[i];
   }
-
   return multiplied;
 };
 
-const divide = function() {
-	values = [...arguments];
+const divide = function(values) {
+  let divided = values[0];
+  for (let i=1; i<values.length; i++){
+    divided /= values[i];
+  }
+  return divided;
+};
 
-    let divided = values[0];
-    for (let i=1; i<values.length; i++){
-      divided /= values[i];
-    }
-  
-    return divided;
-  };
-
-const power = function() {
-	values = [...arguments];
-
+const power = function(values) {
   let powered = Math.pow(values[0], values[1]);
   return powered;
 };
 
-const factorial = function(value) {
-
-  let answer
+// FACTORIAL // 
+const factorial = function(values) {
+  value = values[0];
   if (value === 0){
     answer = 1;
   }
@@ -68,6 +81,8 @@ const factorial = function(value) {
   return answer;
 };
 
+// Recursive function, starting on num, it goes down to 1, then bubbles up and multiplies by every consecutive number
 function FirstFactorial(num) {
   return (num === 1 ? 1 : num * FirstFactorial(num - 1));
 }
+// FACTORIAL //
